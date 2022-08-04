68a58093e90991cf6cff78fd6ec6a3b8c11308bd
Essential Randomness <essential.randomn3ss@gmail.com>
2022-08-03 18:31:46 -0700

    Load stylesheet from gist

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
 import "./App.css";
 
+import React from "react";
+
+const getGistIdFromUrl = (url: string) => {
+  // Url example: https://gist.github.com/essential-randomness/26408ede94ccb51c2074364cab52c3fd
+  return "26408ede94ccb51c2074364cab52c3fd";
+};
+
+const fetchGistContent = async (gistId: string) => {
+  const data = await (
+    await fetch(`https://api.github.com/gists/${gistId}`)
+  ).json();
+
+  return data.files["initial-test.css"].content;
+};
+
+const loadGist = async (url: string) => {
+  const gistId = getGistIdFromUrl(url);
+
+  return await fetchGistContent(gistId);
+};
+
 function App() {
+  const [loading, setLoading] = React.useState(false);
+  const styleElementRef = React.useRef<HTMLStyleElement | null>(null);
+
   return (
     <main>
       <section>
@@ -34,6 +58,28 @@ function App() {
             </dd>
           </dl>
         </details>
+      </section>
+      <section>
+        <h2>Theme me up</h2>
+        <button
+          disabled={loading}
+          onClick={async () => {
+            setLoading(true);
+            const gistContent = await loadGist(
+              "https://gist.github.com/essential-randomness/26408ede94ccb51c2074364cab52c3fd"
+            );
+            if (styleElementRef.current) {
+              document.head.removeChild(styleElementRef);
+            }
+            const styleEl = document.createElement("style");
+            styleEl.textContent = gistContent;
+            document.head.appendChild(styleEl);
+            styleElementRef.current = styleEl;
+            setLoading(false);
+          }}
+        >
+          Test
+        </button>
       </section>
     </main>
   );
